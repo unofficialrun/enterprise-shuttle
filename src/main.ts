14495c136505456b3e7c7db576b47462d1721cd2
christopherwxyz <christopher@unofficial.run>
2024-06-14 10:05:07 -0400

    chore: Refactor port availability checking logic
    The code changes in `src/main.ts` refactor the port availability checking logic by introducing the `checkPort` function. This function uses the `node:net` module to create a server and check if the specified port is available. If the port is available, the Nest application is started on that port. Otherwise, a warning message is logged and the server is not started. This change improves the readability and maintainability of the code.

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CommandFactory } from 'nest-commander';
 import { Logger } from '@nestjs/common';
-import * as portfinder from 'portfinder';
+import * as net from 'node:net';
+
+async function checkPort(port: number): Promise<boolean> {
+  return new Promise((resolve) => {
+    const tester = net.createServer()
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+      .once('error', (err: any) => (err.code === 'EADDRINUSE' ? resolve(false) : resolve(true)))
+      .once('listening', () => tester.once('close', () => resolve(true)).close())
+      .listen(port);
+  });
+}
 
 async function bootstrap() {
-  try {
-    // Check if port 8080 is available
-    const port = await portfinder.getPortPromise({ port: 8080 });
+  const isPortAvailable = await checkPort(8080);
 
-    if (port === 8080) {
-      // Start HTTP server if port 8080 is available
+  if (isPortAvailable) {
+    try {
       const app = await NestFactory.create(AppModule);
       const server = app.listen(8080, '0.0.0.0', () => {
         Logger.log('HTTP server running on http://0.0.0.0:8080');
@@ -20,13 +28,13 @@ async function bootstrap() {
       const commands = CommandFactory.run(AppModule, ["debug", "error", "log", "warn", "fatal", "verbose"]);
 
       await Promise.all([server, commands]);
-    } else {
-      Logger.warn('Port 8080 is already in use. HTTP server will not be started.');
-      // Run command-line functionality without starting the server
-      await CommandFactory.run(AppModule, ["debug", "error", "log", "warn", "fatal", "verbose"]);
+    } catch (error) {
+      Logger.error('Error starting Nest application:', error);
     }
-  } catch (error) {
-    Logger.error('Error checking port 8080:', error);
+  } else {
+    Logger.warn('Port 8080 is already in use. HTTP server will not be started.');
+    // Run command-line functionality without starting the server
+    await CommandFactory.run(AppModule, ["debug", "error", "log", "warn", "fatal", "verbose"]);
   }
 }
 
