e551e942415c93077f2faa64c56de2da66c80dcf
christopherwxyz <christopher@unofficial.run>
2024-06-27 17:32:05 -0700

    refactor: Update insertCasts and deleteCasts functions to use AppDb instead of Kysely<Tables>
    This commit updates the insertCasts and deleteCasts functions in the cast.ts file to use the AppDb type instead of Kysely<Tables>. This change aligns the code with the recent user commits and ensures consistency in the usage of the database connection throughout the application.

--- src/processors/cast.ts
+++ src/processors/cast.ts
@@ -1,15 +1,14 @@
 import { type Message, fromFarcasterTime } from "@farcaster/hub-nodejs";
 
 import { formatCasts } from "./utils.js";
-import type { AppDb, Tables } from "../db.js";
+import type { AppDb } from "../db.js";
 import { log } from "../log.js";
-import { Kysely } from "kysely";
 
 /**
  * Insert casts in the database
  * @param msg Hub event in JSON format
  */
-export async function insertCasts(msgs: Message[], db: Kysely<Tables>) {
+export async function insertCasts(msgs: Message[], db: AppDb) {
   const casts = formatCasts(msgs);
 
   try {
@@ -30,7 +29,7 @@ export async function insertCasts(msgs: Message[], db: Kysely<Tables>) {
  * @param hash Hash of the cast
  * @param change Object with the fields to update
  */
-export async function deleteCasts(msgs: Message[], db: Kysely<Tables>) {
+export async function deleteCasts(msgs: Message[], db: AppDb) {
   try {
     for (const msg of msgs) {
       const data = msg.data;
